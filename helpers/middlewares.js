ef6f3e6b3ab3d713430edf724810c23081b68c16
ross-u <cirkovic.uros.srb@gmail.com>
2020-02-29 17:54:49 +0100

    Update the middleware helper  function name

--- helpers/middlewares.js
+++ helpers/middlewares.js
@@ -1,4 +1,4 @@
-const createError = require('http-errors');
+const createError = require("http-errors");
 
 exports.isLoggedIn = (req, res, next) => {
   if (req.session.currentUser) next();
@@ -10,7 +10,7 @@ exports.isNotLoggedIn = (req, res, next) => {
   else next(createError(403));
 };
 
-exports.validationLoggin = (req, res, next) => {
+exports.validationLogin = (req, res, next) => {
   const { username, password } = req.body;
 
   if (!username || !password) next(createError(400));
--- routes/auth.js
+++ routes/auth.js
@@ -9,12 +9,12 @@ const User = require("../models/user");
 const {
   isLoggedIn,
   isNotLoggedIn,
-  validationLoggin
+  validationLogin
 } = require("../helpers/middlewares");
 
-// GET '/auth/me'
-//  - check if the user IS logged in using helper function (check if session exists)
-//  - if yes, send the response with user info (available on req.session.currentUser)
+// POST '/auth/signup'
+//  - check that user is NOT logged in - using helper function (check if req.session.currentUser exists)
+//  - check that `password` and the `username` are sent - using helper function
 
 // POST '/auth/login'
 //  - check that user is NOT logged in - using helper function (check if req.session.currentUser exists)
@@ -23,19 +23,19 @@ const {
 //  - if user doesn't exist - forward the error to the error middleware using `next()`
 //  - if user exists get assign the user document to `req.session.currentUser` and then send  json response
 
-// POST '/auth/signup'
-//  - check that user is NOT logged in - using helper function (check if req.session.currentUser exists)
-//  - check that `password` and the `username` are sent - using helper function
+// POST '/auth/logout'
+//  - check if the user is logged in using helper function (check if session exists)
+//  - destroy the session
+//  - set status code and send the response back
+
+// GET '/auth/me'
+//  - check if the user IS logged in using helper function (check if session exists)
+//  - if yes, send the response with user info (available on req.session.currentUser)
 
 // - check if `username` already exists in the DB
 //  - if username exists - forward the error to the error middleware using `next()`
 //  - else if username doesn't exists hash the password and create new user in the DB
 //  - then assign the  new user document to `req.session.currentUser` and then send  json response
-
-// POST '/auth/logout'
-//  - check if the user IS logged in using helper function (check if session exists)
-//  - destroy the session
-//  - set status code and send the response back
 
 // GET '/auth/private'   --> Only for testing - Same as /me but it returns a message instead
 //  - check if the user IS logged in using helper function (check if session exists)
