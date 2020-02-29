88e4d1aa210fd8b8e15ee5adf0a1d1ca81f3c102
ross-u <cirkovic.uros.srb@gmail.com>
2020-02-29 18:00:54 +0100

    Update comments

--- routes/auth.js
+++ routes/auth.js
@@ -15,13 +15,20 @@ const {
 // POST '/auth/signup'
 //  - check that user is NOT logged in - using helper function (check if req.session.currentUser exists)
 //  - check that `password` and the `username` are sent - using helper function
+//  - check if the `username` exists, if it does send a response with an error
+//  - if `username` is unique then:
+//     - encrypt the password using bcrypt
+//     - create the new user in DB using the `username` and the encrypted password
+//     - save the newly created user in the `session`
+//     - send back the response 201 (created) and the new user object
 
 // POST '/auth/login'
 //  - check that user is NOT logged in - using helper function (check if req.session.currentUser exists)
 //  - check that `password` and `username` are sent - using helper function
 //  - check if user exists in the DB
 //  - if user doesn't exist - forward the error to the error middleware using `next()`
-//  - if user exists get assign the user document to `req.session.currentUser` and then send  json response
+//  - if user exists assign the user document to `req.session.currentUser`
+//  - send  json response
 
 // POST '/auth/logout'
 //  - check if the user is logged in using helper function (check if session exists)
