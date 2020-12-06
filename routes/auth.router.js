b14d3519f5de28a3191d5f24c6b0e602cd585813
ross-u <cirkovic.uros.srb@gmail.com>
2020-12-06 22:22:27 +0100

    Update comments

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -13,39 +13,35 @@ const {
 } = require("../helpers/middlewares");
 
 // POST '/auth/signup'
-//  - check that user is NOT logged in - using helper function (check if req.session.currentUser exists)
-//  - check that `password` and the `username` are sent - using helper function
-//  - check if the `username` exists, if it does send a response with an error
-//  - if `username` is unique then:
+//  - check that user is NOT logged in - using middleware (check if `req.session.currentUser` is set)
+//  - check that `password` and the `username` are sent in the req.body - using middleware
+//  - check if the `username` is already taken, if it is send an error response - forward the error to the error middleware using `next()`
+//  - if `username` is unique (not taken) then:
 //     - encrypt the password using bcrypt
 //     - create the new user in DB using the `username` and the encrypted password
-//     - save the newly created user in the `session`
-//     - send back the response 201 (created) and the new user object
+//     - set the `req.session` using newly created user object, to trigger creation of the session
+//     - send json response (new user object) with the status code 201 (Created).
 
 // POST '/auth/login'
-//  - check that user is NOT logged in - using helper function (check if req.session.currentUser exists)
-//  - check that `password` and `username` are sent - using helper function
-//  - check if user exists in the DB
-//  - if user doesn't exist - forward the error to the error middleware using `next()`
-//  - if user exists assign the user document to `req.session.currentUser`
-//  - send  json response
+//  - check that user is NOT logged in - using middleware (check if `req.session.currentUser` is set)
+//  - check that `password` and the `username` are sent in the req.body - using middleware
+//  - check if user with the same `username` exists in the DB
+//  - if user doesn't exist send an error response - forward the error to the error middleware using `next()`
+//  - if user exists - set the `req.session`, to trigger creation of the session
+//  - send json response with the status code 200 (OK)
 
 // POST '/auth/logout'
-//  - check if the user is logged in using helper function (check if session exists)
+//  - check if the user is logged in using middleware (check if `req.session.currentUser` is set)
 //  - destroy the session
-//  - set status code and send the response back
+//  - set status code 204 (No Content) and send the response back
+
+
+// GET '/auth/private'   --> Same as /me but it returns a message instead (Example only, no real use)
+//  - check if the user IS logged in using middleware (check if `req.session.currentUser` is set)
+//  - set status code and send the json message response back
 
 // GET '/auth/me'
-//  - check if the user IS logged in using helper function (check if session exists)
-//  - if yes, send the response with user info (available on req.session.currentUser)
-
-// - check if `username` already exists in the DB
-//  - if username exists - forward the error to the error middleware using `next()`
-//  - else if username doesn't exists hash the password and create new user in the DB
-//  - then assign the  new user document to `req.session.currentUser` and then send  json response
-
-// GET '/auth/private'   --> Only for testing - Same as /me but it returns a message instead
-//  - check if the user IS logged in using helper function (check if session exists)
-//  - set status code and send the json message response back
+//  - check if the user Is logged in using middleware (check if `req.session.currentUser` is set)
+//  - if yes, send the response with user info (available on `req.session.currentUser`)
 
 module.exports = router;
